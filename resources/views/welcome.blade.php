0631c6b598e6eab15b80de238680c6dbcca56624
Michael Olufemi <michealolufemi53@yahoo.com>
2017-06-26 05:04:56 -0700

    views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
 <meta name="robots" content="noindex, nofollow">
 <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0">
 
-<link rel="stylesheet" id="css-main" href="assets/css/oneui.min-3.2.css">
+<link rel="stylesheet" id="css-main" href="assets/css/oneui.css">
 </head>
 <body><div class="content bg-white text-center pulldown overflow-hidden">
 <div class="row">
@@ -47,6 +47,4 @@
 Powered By:<br>
 <a class="link-effect" href="/">Crypto2Cedis</a>
 </div>
-<script src="assets/js/oneui.min-3.2.js"></script><script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-16158021-6', 'auto');ga('send', 'pageview');</script>
-</body>
 </html>
